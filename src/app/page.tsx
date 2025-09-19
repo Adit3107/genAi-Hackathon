56628b5d8a9ff4054a8830b9b976ec744a85e820
Aditya Kalmegh <adityakalmegh01@gmail.com>
2025-09-19 08:58:25 +0000

    use type text for hero text from react bits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Image from 'next/image';
 import { ArrowRight } from 'lucide-react';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
   const heroImage = PlaceHolderImages.find((img) => img.id === 'home-hero');
@@ -11,9 +13,22 @@ export default function Home() {
     <div className="container mx-auto px-4 py-16 sm:py-24">
       <section className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12">
         <div className="space-y-6 text-center lg:text-left">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-headline tracking-tighter">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-headline tracking-tighter h-36 lg:h-44">
             Navigate Your Career Path with{' '}
-            <span className="text-primary">Confidence</span>
+            <TypeAnimation
+              sequence={[
+                'Confidence',
+                2000,
+                'Clarity',
+                2000,
+                'Success',
+                2000,
+              ]}
+              wrapper="span"
+              speed={50}
+              className="text-primary"
+              repeat={Infinity}
+            />
           </h1>
           <p className="max-w-xl mx-auto lg:mx-0 text-lg text-muted-foreground">
             CareerCompass helps you understand your skills, discover new
